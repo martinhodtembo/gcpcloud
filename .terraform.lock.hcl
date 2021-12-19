afa282a29eb6180aaebd68bd3f9776cc7004ed14
cmuriuki <c.muriukin@gmail.com>
2021-12-19 17:34:24 +0300

    All files added

---
+++ .terraform.lock.hcl
@@ -0,0 +1,40 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/google" {
+  version     = "3.5.0"
+  constraints = "3.5.0"
+  hashes = [
+    "h1:seOBe8JM4Wzh44i0fqhrKpv9P3pP1lyulZJvTkyLD5w=",
+    "zh:1bd907d58a05ec25d48294f6a0d7ab215d7243daa8a100ee983981ca641d2ce9",
+    "zh:334c0ac7599da444434d5cd1045171b8decf4eec1d01a69dd9a343e62ca8ffc0",
+    "zh:738c0808e29d14e9508f121c1966fa557b3db83fb5b0ce9b32ffebfbcc610dbe",
+    "zh:8b4c910d558d3176f96c50bca9c3484edd0df2a7429bf1609cd5aca2cf1aeab8",
+    "zh:a1e3d0057158267c2d86a5fc46cfa50e86cf70a60f242a96c66710c48d97bc04",
+    "zh:a718d621c671cb6d1d951e070ed9f52710a2441fa14e1aee7c00e14396142f9a",
+    "zh:ac1381f6e42de469a2842af8ba110787c9a7c1a1b250a53fe0831b70f54a56f6",
+    "zh:af979d49936061c0c37c1df7ddfbf1bca6b1f347562c8917b4e063ac7c354af7",
+    "zh:af9a7e94706d4155f88b2547620edc2ebfeaf2c4bb8f670a75e04ab7470c6640",
+    "zh:d820fb164d3f7da15aa4963a59f05134362b28c983d0cfc30ee3e325bc3362cb",
+    "zh:e1cd06c6ed45dd0d644b4afec7b8499e7be6337d45f4b802c933dd0016efc326",
+    "zh:fe6e7ea5f1a1d7876ac9939eb8cfc1376536d25ad7ecc86f5d6660c5b09a9de3",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/google-beta" {
+  version = "4.4.0"
+  hashes = [
+    "h1:5IX7TadmigN9SxrDtlM3L7JOQWU0s5CjmchqbVeXwng=",
+    "zh:083f1f42bf9f7ca79c93122580ce7c24fdefbe057088bf8d4fd1087c2646183e",
+    "zh:2bc7af401f71993872d1b5d6aab7e47c7931a8b771a09b93674422e7e63ed0aa",
+    "zh:4ce8231a05157f1bee4ec3e59e485a43e4b2259e6e2f3f10ae19d00c46e87aa6",
+    "zh:550e93cdb125b54e65d28c3ea3577324ed2cbe0177d26fd1922574629a992e40",
+    "zh:58149311372b3a838c9e7cce819489c82c0950cb25cd32da5c967452b106a3d9",
+    "zh:6bc5cd3ce392c88b7cf3119ae19cf982aee73e0d00ca42b2cd444781063b7a1d",
+    "zh:982f9c39a05c793d3e2789410eb53b30d87981e98450d792dbceea06f097f9d2",
+    "zh:a0c7ba877e3f6732fbcdb26dbf1b4e48a65fcc147687209082e2459c0942ab46",
+    "zh:b4fe174d00d4d1d9ca1b49891e078f168f0e5d6a82d50f0a0bec88eeebb5a1f7",
+    "zh:b860bff0fcb04ed51c2d279337d0df3d8d451d62aa39bbcfa524805f259afeed",
+    "zh:dc823c4d7af3bb8420a14951ea501a0537f2b2067eb2d2be1655428ff0be57d6",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,90 @@
+provider "google" {
+version = "3.5.0"
+credentials = file("C:/Users/cnyaga/Desktop/gcpcloud/infonation-008976ff00ba.json")
+project = "infonation"
+region = "${var.region}"
+zone = "${var.region}-a"
+}
+
+
+resource "google_compute_firewall" "firewall" {
+  name    = "gritfy-firewall-externalssh"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"] # Not So Secure. Limit the Source Range
+  target_tags   = ["externalssh"]
+}
+resource "google_compute_firewall" "webserverrule" {
+  name    = "gritfy-webserver"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports    = ["80","443"]
+  }
+  source_ranges = ["0.0.0.0/0"] # Not So Secure. Limit the Source Range
+  target_tags   = ["webserver"]
+}
+# We create a public IP address for our google compute instance to utilize
+resource "google_compute_address" "static" {
+  name = "vm-public-address"
+  project = var.project
+  region = "${var.region}"
+  depends_on = [ google_compute_firewall.firewall ]
+}
+
+resource "google_compute_disk" "disk" {
+    name  = "my-disk"
+    image = "centos-cloud/centos-8"
+    size  = 50
+    type  = "pd-ssd"
+    zone  = "us-central1-a"
+}
+resource "google_compute_instance" "dev" {
+  name         = "devserver"
+  machine_type = "f1-micro"
+  zone         = "${var.region}-a"
+  tags         = ["externalssh","webserver"]
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-8"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = google_compute_address.static.address
+    }
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      host        = google_compute_address.static.address
+      type        = "ssh"
+      user        = var.user
+      timeout     = "500s"
+      private_key = file(var.privatekeypath)
+    }
+    inline = [
+      "sudo yum -y install epel-release",
+      "sudo yum -y install nginx",
+      "sudo nginx -v",
+      "yum install cloud-init",
+      "yum install Ansible",
+      "sudo cloud-init init",
+      "echo 'Challenge: Automation end to end' > /usr/share/nginx/www/index.html",
+      "docker run -d -p 80:80 --name automation-end2end -v /usr/share/nginx/www/index.html:/usr/share/nginx/www/index.html --restart=always nginx"
+          ]
+  }
+  # Ensure firewall rule is provisioned before server, so that SSH doesn't fail.
+  depends_on = [ google_compute_firewall.firewall, google_compute_firewall.webserverrule ]
+  service_account {
+    email  = var.email
+    scopes = ["compute-ro"]
+  }
+  metadata = {
+    ssh-keys = "${var.user}:${file(var.publickeypath)}"
+  }
+}
